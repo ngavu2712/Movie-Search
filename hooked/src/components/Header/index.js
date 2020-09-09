69911dbc1adb47c3aa0456625a7f3f003e778a8a
Anna Vu <ngavu2712@gmail.com>
2020-09-08 22:13:53 -0700

    update file path for App in index.js

--- hooked/src/components/Header/index.js
+++ hooked/src/components/Header/index.js
@@ -4,7 +4,7 @@ import React from "react"
 const Header = (props) => {
     return(
         <header className="App-header">
-            {/* This is a functional compoenet that renders header tag wiht the text prop */}
+            {/* This is a functional component that renders header tag with the text prop */}
             <h2>{props.text}</h2>
         </header>
     )
