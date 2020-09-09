9dd2b5d56f08dc96d605ccb87b64cf5c12b99279
Anna Vu <ngavu2712@gmail.com>
2020-09-09 16:03:47 -0700

    rearrange code

--- hooked/src/components/App/App.js
+++ hooked/src/components/App/App.js
@@ -4,51 +4,60 @@ import Header from "../Header"
 import Movie from "../Movie"
 import Search from "../Search"
 
-const MOVIE_API_URL ="http://www.omdbapi.com/?s=tt3896198&apikey=3a51762f"
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=3a51762f"
+
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-  const [loading,setLoading] = useState(true)
-  const [movies, setMovies] = useState([])
-  const [errorMessage, setErrorMessage] =useState(null)
-
-    useEffect (() => {
-
-      fetch(MOVIE_API_URL)
-        .then(res => res.json())
-        .then(jsonRes => {
-          setMovies(jsonRes.Search)
-          setLoading(false)
-        })
-    }, [] )
+    useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setMovies(jsonResponse.Search);
+        setLoading(false);
+      });
+  }, []);
 
     const search = searchValue => {
-      setLoading(true)
-      setErrorMessage(null)
-      
-        fetch(`http://www.omdbapi.com/?s=${searchValue}&apikey=3a51762f`)
-          .then(res => res.json())
-          .then(jsonRes => {
-            if(jsonRes.res === "True") {
-              setMovies(jsonRes.Search)
-              setLoading(false)
-            } else {
-              setErrorMessage(jsonRes.Error)
-              setLoading(false)
-            }
-          })
-    }
+    setLoading(true);
+    setErrorMessage(null);
 
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response === "True") {
+          setMovies(jsonResponse.Search);
+          setLoading(false);
+        } else {
+          setErrorMessage(jsonResponse.Error);
+          setLoading(false);
+        }
+      });
+  	};
+
+    
     return (
-      <div className="App">
-        <Header text="HOOKED"/>
-        <Search search={search} />
-        <p className="App-intro">Sharing Your Favorite Movies</p>
-        <div className="movies">
-          {loading && !errorMessage ? (<span>loading...</span>) : errorMessage ? (<div className="errorMessage"></div>) : (movies.map((movie, i)=>(<Movie key={`${i}-${movie.Title}`} movie={movie}/>)))}
-        </div>
+     <div className="App">
+      <Header text="MOVIE NIGHT" />
+      <Search search={search} />
+      <p className="App-intro">Sharing Your Favorite Movies</p>
+      <div className="movies">
+        {loading && !errorMessage ? (
+         <span>loading...</span>
+         ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map((movie, index) => (
+            <Movie key={`${index}-${movie.Title}`} movie={movie} />
+          ))
+        )}
       </div>
-    )
-}
+    </div>
+  );
+};
+
 
 export default App;
