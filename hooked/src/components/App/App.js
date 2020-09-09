12eca52c227004b80b83f25edb9a8b5274f9ee69
Anna Vu <ngavu2712@gmail.com>
2020-09-09 15:38:22 -0700

    add hooks to app component

--- hooked/src/components/App/App.js
+++ hooked/src/components/App/App.js
@@ -2,25 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MOVIE_API_URL ="http://www.omdbapi.com/?s=tt3896198&apikey=3a51762f"
+
+const App = () => {
+
+  const [loading,setLoading] = useState(true)
+  const [movies, setMovies] = useState([])
+  const [errorMessage, setErrorMessage] =useState(null)
+
+    useEffect (() => {
+
+      fetch(MOVIE_API_URL)
+        .then(res => res.json())
+        .then(jsonRes => {
+          setMovies(jsonRes.Search)
+          setLoading(false)
+        })
+    }, [] )
+
+    const search = searchValue => {
+      setLoading(true)
+      setErrorMessage(null)
+      
+        fetch(`http://www.omdbapi.com/?s=${searchValue}&apikey=3a51762f`)
+          .then(res => res.json())
+          .then(jsonRes => {
+            if(jsonRes.res === "True") {
+              setMovies(jsonRes.Search)
+              setLoading(false)
+            } else {
+              setErrorMessage(jsonRes.Error)
+              setLoading(false)
+            }
+          })
+    }
+
+    return (
+      <div className="App">
+        <Header text="HOOKED"/>
+        <Search search={search} />
+        <p className="App-intro">Sharing Your Favorite Movies</p>
+        <div className="movies">
+          {loading && !errorMessage ? (<span>loading...</span>) : errorMessage ? (<div className="errorMessage"></div>) : (movies.map((movie, i)=>(<Movie key={`${i}-${movie.Title}`} movie={movie}/>)))}
+        </div>
+      </div>
+    )
 }
 
 export default App;
--- hooked/src/components/Search/index.js
+++ hooked/src/components/Search/index.js
@@ -2,12 +2,12 @@ import React , {useState} from 'react'
 
 const Search = (props) => {
 
-    const [searchValue, setSearchValue] = useState("");
-    const handleSearchInputChanges = (e) => {
+    const [searchValue, setSearchValue] = useState(""); //this.state
+    const handleSearchInputChanges = (e) => { //update the state with new value
         setSearchValue(e.target.value);
     }
 
-    const resetInputField = () => {
+    const resetInputField = () => { //call the state update function with an empty string
         setSearchValue ("")
     }
 
